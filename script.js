deb790ffb584dd9c4274e6be2d0629858d9afcec
ShivamSinghh9 <shivam132e@gmail.com>
2026-03-06 18:24:45 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const bookBtns = document.querySelectorAll(".bookTicketBtn");
+const ticketForm = document.getElementById("ticketForm");
+const closeForm = document.getElementById("closeForm");
+
+bookBtns.forEach(function(btn){
+    btn.addEventListener("click", function(){
+        ticketForm.classList.add("active");
+    });
+});
+
+closeForm.addEventListener("click", function(){
+    ticketForm.classList.remove("active");
+});
